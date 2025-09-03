927d244b60fbe332e3de0dd801d82a2eb9413654
ThienVo <votrungthien1503@gmail.com>
2025-09-03 22:15:47 +0200

    fix the dist path problem by inserting base to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/all_in_one_frontend',
+    base: './', // <-- set this to your subfolder
 });
